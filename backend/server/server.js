8985a84720f089b9d745615f19bcf6f919e36f0d
rashedWeAreDamage <rashed@wearedamage.com>
2019-06-26 14:01:20 +0600

    Server .js

--- backend/server/server.js
+++ backend/server/server.js
@@ -4,7 +4,6 @@ import db from '../db';
 import posts from '../routes/api/posts';
 import user from '../routes/api/users';
 
-
 const dbUri = 'mongodb://localhost/playground';
 
 // Initialize express
@@ -17,9 +16,7 @@ db.connect(dbUri, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => res.json({ msg: "hello " }));
-
+// Posts and User routes handler
 app.use('/api/posts', posts);
 app.use('/api/user', user);
 

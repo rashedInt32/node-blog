deb50635240c9b1bf7b7bf09b37a45b0c120375d
rashedInt32 <parvez08eee@gmail.com>
2019-06-30 22:21:56 +0600

    login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Input from '../components/forms/Input';
+
+const url = 'http://localhost:3900/api/user/login'
 
 const Login = () => {
   const [authData, setAuthData] = useState({
@@ -9,12 +12,21 @@ const Login = () => {
 
   const onInputChange = (e) => {
     const { name, value } = e.target;
-    setAuthData({ [name]: value });
+    setAuthData({ ...authData, [name]: value });
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
   }
 
   return (
     <div className="row">
-      <form action="#" className="col-md-4 offset-md-4">
+      <form
+        action="#"
+        className="col-md-4 offset-md-4"
+        onSubmit={onSubmitForm}
+      >
         <Input
           type="email"
           label="Email"

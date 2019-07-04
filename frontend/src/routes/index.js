5f8e4784af98ddb9b4861568d35e92c8fb77dac7
rashedInt32 <parvez08eee@gmail.com>
2019-07-04 21:55:45 +0600

    Some refactoring over header

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 
+import Header from '../components/header/Header';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Posts from '../pages/Posts';
 
-const Routes = (props) =>
-  <Router>
-    {props.children}
-    <Route path="/login" component={Login} />
-    <Route path="/register" component={Register} />
-    <PrivateRoute path="/posts" component={Posts} />
-  </Router>
+import { config } from '../config';
+const url = `${config.url}/user/logout`;
+
+const Routes = (props) => {
+  const handleLogOut = async () => {
+    const { data } = await axios.get(url);
+    localStorage.setItem('app-token', data.token);
+    localStorage.removeItem('user');
+  };
+
+  return (
+    <Router>
+      <Header onLogout={handleLogOut} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <PrivateRoute path="/posts" component={Posts} />
+    </Router>
+  )
+}
 
 export default Routes;
